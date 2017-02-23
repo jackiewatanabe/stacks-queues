37c277d8915543ceaf52e39260b1884ca3d1698e
Jackie Watanabe <sushiigirl@gmail.com>
2017-02-23 12:36:42 -0800

    created implementations for stacks and queues

--- lib/Stack.rb
+++ lib/Stack.rb
@@ -4,18 +4,24 @@ class Stack
   end
 
   def push(element)
+    @store.push(element)
+    # @store.unshift(element)
   end
 
   def pop
+    return @store.pop
   end
 
   def top
+    @store[-1]
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0
   end
 
   def to_s
